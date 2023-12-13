bf4378da34956901bfad3078fa3e30c7eaf1f1d4
Michael Schurter <mschurter@hashicorp.com>
2023-12-13 14:52:48 -0800

    use _member not _policy

--- gcp/gcs.tf
+++ gcp/gcs.tf
@@ -19,67 +19,9 @@ resource "google_storage_bucket_acl" "nomad" {
   bucket = google_storage_bucket.nomad.name
 }
 
-data "google_iam_policy" "nomad_gcs" {
-  binding {
-    role = "roles/storage.admin"
-
-    members = [
-      google_service_account.nomad.member
-    ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "nomad" {
+resource "google_storage_bucket_iam_member" "nomad" {
   bucket = google_storage_bucket.nomad.name
 
-  policy_data = data.google_iam_policy.nomad_gcs.policy_data
-
-  #  policy_data = <<POLICY
-  #{
-  #  "bindings": [
-  #    {
-  #      "members": [
-  #        "serviceAccount:manual-nomad-test@hc-4ce4645b798f4ced93ce1f8aed2.iam.gserviceaccount.com"
-  #      ],
-  #      "role": "roles/storage.admin"
-  #    },
-  #    {
-  #      "members": [
-  #        "principal://iam.googleapis.com/projects/960872035951/locations/global/workloadIdentityPools/nomad-pool-9/subject/global:default:example:cache:redis:test",
-  #        "principal://iam.googleapis.com/projects/960872035951/locations/global/workloadIdentityPools/nomad-pool-9/subject/global:default:gcs:gcs:gcs:test",
-  #        "projectEditor:hc-4ce4645b798f4ced93ce1f8aed2",
-  #        "projectOwner:hc-4ce4645b798f4ced93ce1f8aed2",
-  #        "serviceAccount:manual-nomad-test@hc-4ce4645b798f4ced93ce1f8aed2.iam.gserviceaccount.com"
-  #      ],
-  #      "role": "roles/storage.legacyBucketOwner"
-  #    },
-  #    {
-  #      "members": [
-  #        "projectViewer:hc-4ce4645b798f4ced93ce1f8aed2"
-  #      ],
-  #      "role": "roles/storage.legacyBucketReader"
-  #    },
-  #    {
-  #      "members": [
-  #        "projectEditor:hc-4ce4645b798f4ced93ce1f8aed2",
-  #        "projectOwner:hc-4ce4645b798f4ced93ce1f8aed2",
-  #        "serviceAccount:manual-nomad-test@hc-4ce4645b798f4ced93ce1f8aed2.iam.gserviceaccount.com"
-  #      ],
-  #      "role": "roles/storage.legacyObjectOwner"
-  #    },
-  #    {
-  #      "members": [
-  #        "projectViewer:hc-4ce4645b798f4ced93ce1f8aed2"
-  #      ],
-  #      "role": "roles/storage.legacyObjectReader"
-  #    },
-  #    {
-  #      "members": [
-  #        "serviceAccount:manual-nomad-test@hc-4ce4645b798f4ced93ce1f8aed2.iam.gserviceaccount.com"
-  #      ],
-  #      "role": "roles/storage.objectCreator"
-  #    }
-  #  ]
-  #}
-  #POLICY
+  role = "roles/storage.admin"
+  member = google_service_account.nomad.member
 }
--- gcp/iam.tf
+++ gcp/iam.tf
@@ -35,7 +35,7 @@ resource "google_service_account_iam_binding" "nomad" {
     #FIXME google_workload_identity_pool seems to lack an attribute for the
     #      principal, so string format it manually to look like:
     #principal://iam.googleapis.com/projects/PROJECT_NUM/locations/global/workloadIdentityPools/POOL_NAME/subject/SUBJECT_MAPPING
-    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.nomad.name}"
+    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.nomad.name}/subject/global:default:gcs:gcs:gcs:test"
   ]
 
   depends_on = [
